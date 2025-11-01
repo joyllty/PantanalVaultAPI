4202e913926f9a76fd45757a0f6c84b90da3d1f3
Bruna Chapelin <bruna.polvina@gmail.com>
2025-11-01 14:37:33 -0300

    remoção da propriedade colaboradores da tabela album

---
+++ Migrations/20251101173523_remove_propriedade_colaboradores.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PantanalVaultAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class removepropriedadecolaboradores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Colaboradores",
+                table: "Albuns");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Colaboradores",
+                table: "Albuns",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
--- dados.sql
+++ dados.sql
@@ -1,23 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Net.Http.Headers;
-
-namespace PantanalVaultAPI
-{
-    public class Album
-    {
-        public int Id { get; set; }
+public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Artistas { get; set; }
         public string? Generos { get; set; }
+        public string? Colaboradores { get; set; }
         public string? DataLancamento { get; set; }
         public int NumeroFaixas { get; set; }
         public string? Duracao { get; set; }
         public string? Gravadora { get; set; }
         public string? Formato { get; set; }
-
-    }
-}
-
+(Nome, Artistas, Generos, Colaboradores, DataLancamento, 
