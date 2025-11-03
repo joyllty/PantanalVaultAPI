e47453f1c7e809eb99e79682c370a936e9a3535a
Lanna <lannadosanjoscosta@gmail.com>
2025-11-03 09:01:54 -0300

    primeira leva de musicas

---
+++ dados.sql
@@ -0,0 +1,11 @@
+INSERT INTO Albuns (Nome, Artistas, Generos, Colaboradores, DataLancamento, NumeroFaixas, Duracao, Gravadora, Formato) VALUES
+('Crash', 'Charli XCX', 'Pop', 'Christine and The Queens,Caroline Polachek,Rina Sawayama', '2022-03-18', 12, '33:51', 'Asylum Records UK', 'Álbum'),
+('The Jaws of Life', 'Pierce The Veil', 'Rock Alternativo', 'Chloe Moriondo', '2023-02-10', 12, '41:31', 'Fearless Records', 'Álbum'),
+('Miss Narcissist', 'Courtney Love', 'Rock, Alternative Rock', NULL, '2015-05-19', 1, '3:08', 'Ghost Ramp/Crush Music', 'Single'),
+('BAILE', 'FBC,VHOOR', 'Hiphop,Funk', 'UANA, Ferb, Djair Voz Cristalina, Mac Júlia, Mariana Cavanellas', '2021-11-12', 10, '27:07', 'FBC & VHOOR, A União da Força e da Fé', 'Álbum'),
+('PIXELATED KISSES', 'Joji', 'Alternativo,Indie', NULL, '2025-10-14', 1, '1:50', 'Palace Creek', 'Single'),
+('There is a Hell Believe Me I´ve Seen it. There is a Heaven Let´s Keep It a Secret', 'Bring me The Horizon', 'Metalcore,Metal', 'Lights, Josh Franceschi, Josh Scogin', '2010-10-04', 12, '52:56', 'BMG Rights Management UK', 'Álbum'),
+('Bury Me At Makeout Creek', 'Mitski', 'Alternativo', NULL, '2014-11-11', 10, '30:18', 'Dead oceans', 'Álbum'),
+('The Divine Feminine', 'Mac Miller', 'Hiphop Alternativo,Neo Soul,Jazz Rap', 'Bilal,Anderson .Paak, Ty Dolla $ign,NJOMZA, CeeLo Green, Ariana Grande, Kendrick Lamar', '2016-09-16', 10, '52:37', 'Warner Records', 'Álbum'),
+('Pirata', 'Maria Bethânia', 'MPB, Samba, Pagode', NULL, '2006-01-01', 15, '43:19', 'Quitanda', 'Álbum'),
+('The Essential Skeeter Davis', 'Skeeter Davis', 'Country Clássico, Folk Tradicional', 'The Davis Sisters', '1994-05-23', 20, '48:54', 'BMG Entertainment', 'Compilação');
