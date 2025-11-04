cd752d98e443e443503d8f71e3af52a3010439d3
Lanna <lannadosanjoscosta@gmail.com>
2025-11-03 23:44:32 -0300

    post e patch

--- front-end/script.js
+++ front-end/script.js
@@ -5,7 +5,29 @@ const apiURL = 'http://localhost:5287/albuns'
 // const getAlbuns
 
 // POST
-// const postAlbum
+const postAlbum = async (novoAlbum) => {
+    try {
+        const response = await fetch(apiURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novoAlbum)
+        });
+        
+        if (!response.ok) {
+            throw new Error("Erro ao criar álbum!");
+        }
+
+        const albumCriado = await response.json();
+        console.log("Álbum criado: ", albumCriado);
+        return albumCriado;
+
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
 
 // PUT
 const putAlbum = async (id, albumAtualizado) => {
@@ -47,5 +69,34 @@ const deleteAlbum = async (id) => {
         // getAlbuns();
     } catch (error){
         console.error(error);
+    }
+};
+
+// PATCH
+const patchAlbum = async (id, albumAlteracoes) => {
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(albumAlteracoes)
+        });
+        
+        if (response.status === 404) {
+            throw new Error("Álbum não encontrado!");
+        }
+
+        if (!response.ok) {
+            throw new Error("Erro ao atualizar o álbum!");
+        }
+
+        const resultado = await response.json();
+        console.log("Álbum atualizado (PATCH): ", resultado);
+        return resultado;
+
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 };
